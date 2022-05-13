668efa73a884e1cb800f6e261701a57dc33c4343
iamrishav <rishav673@gmail.com>
2022-05-13 13:32:04 +0530

    Online server added

--- kanbanboard/app/src/main/java/com/example/myapplication/ServerURL.java
+++ kanbanboard/app/src/main/java/com/example/myapplication/ServerURL.java
@@ -1,25 +1,25 @@
 package com.example.myapplication;
 
 public class ServerURL {
-    public static String login = "http://10.0.2.2:5000/login";
-    public static String register = "http://10.0.2.2:5000/register";
-    public static String fetchUser = "http://10.0.2.2:5000/fetchuser";
-    public static String forgot = "http://10.0.2.2:5000/forgot";
-    public static String getWorkspaces = "http://10.0.2.2:5000/workspaces";
-    public static String createWorkspace = "http://10.0.2.2:5000/createworkspace";
-    public static String deleteWorkspace = "http://10.0.2.2:5000/deleteworkspace";
-    public static String getWorkspaceTasks = "http://10.0.2.2:5000/workspacetasks";
-    public static String createWorkspaceTask = "http://10.0.2.2:5000/createworkspacetask";
-    public static String updateWorkspaceTask = "http://10.0.2.2:5000/updateworkspacetask";
-    public static String deleteWorkspaceTask = "http://10.0.2.2:5000/deleteworkspacetask";
-    public static String addCollaborators = "http://10.0.2.2:5000/addcollaborators";
-    public static String showcollaborators = "http://10.0.2.2:5000/showcollaborators";
-    public static String removecollab = "http://10.0.2.2:5000/removecollab";
-    public static String addComment = "http://10.0.2.2:5000/addcomment";
-    public static String fetchComments = "http://10.0.2.2:5000/fetchcomments";
-    public static String deleteComment = "http://10.0.2.2:5000/deletecomment";
-    public static String getUsers = "http://10.0.2.2:5000/getUsers";
-    public static String getcollab = "http://10.0.2.2:5000/getcollab";
+    public static String login = "http://rishav673.pythonanywhere.com/login";
+    public static String register = "http://rishav673.pythonanywhere.com/register";
+    public static String fetchUser = "http://rishav673.pythonanywhere.com/fetchuser";
+    public static String forgot = "http://rishav673.pythonanywhere.com/forgot";
+    public static String getWorkspaces = "http://rishav673.pythonanywhere.com/workspaces";
+    public static String createWorkspace = "http://rishav673.pythonanywhere.com/createworkspace";
+    public static String deleteWorkspace = "http://rishav673.pythonanywhere.com/deleteworkspace";
+    public static String getWorkspaceTasks = "http://rishav673.pythonanywhere.com/workspacetasks";
+    public static String createWorkspaceTask = "http://rishav673.pythonanywhere.com/createworkspacetask";
+    public static String updateWorkspaceTask = "http://rishav673.pythonanywhere.com/updateworkspacetask";
+    public static String deleteWorkspaceTask = "http://rishav673.pythonanywhere.com/deleteworkspacetask";
+    public static String addCollaborators = "http://rishav673.pythonanywhere.com/addcollaborators";
+    public static String showcollaborators = "http://rishav673.pythonanywhere.com/showcollaborators";
+    public static String removecollab = "http://rishav673.pythonanywhere.com/removecollab";
+    public static String addComment = "http://rishav673.pythonanywhere.com/addcomment";
+    public static String fetchComments = "http://rishav673.pythonanywhere.com/fetchcomments";
+    public static String deleteComment = "http://rishav673.pythonanywhere.com/deletecomment";
+    public static String getUsers = "http://rishav673.pythonanywhere.com/getUsers";
+    public static String getcollab = "http://rishav673.pythonanywhere.com/getcollab";
 
 
 //    public static String login = "http://192.168.59.84:8000/login";
